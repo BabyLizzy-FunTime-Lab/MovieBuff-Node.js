e5afce31cd9ff269318799998ec402e986360f4b
Darthporkius <cholochullo@hotmail.com>
2021-07-06 15:15:26 +0200

    this does not work yet, babel parts have been commented out for now

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const babel = require('@babel/core');
 const app = express();
 const port = 3000;
 
@@ -8,8 +10,24 @@ app.use(express.urlencoded({
 	extended: true
 }));
 
+// Transform file from js-src into lib
+// babel.transformFileAsync("js-src/main.js").then(result => {
+// 	fs.writeFile('lib/main.js', result.code, function (err) {
+//   	if (err) return console.log(err);
+// 	});
+// });
+// Nodemon keeps reloading
+
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
+	// fs.readFile('lib/main.js', 'utf8' , (err, data) => {
+	//   if (err) {
+ //  	  console.error(err)
+ //  	  return
+ //  	}
+	// 	res.write(data);
+	// 	res.end();
+	// })
 });
 
 app.listen(port, function() {
